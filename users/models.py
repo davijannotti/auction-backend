78db26d2e1eba5a87238cf65ccec6160e8f38f82
davijannotti <davi29012004@gmail.com>
2025-08-04 09:13:31 -0300

    Set default value for User.birth_date to today

--- users/models.py
+++ users/models.py
@@ -1,13 +1,13 @@
 from django.contrib.auth.models import AbstractUser
 from django.db import models
+import datetime
 
 
 class User(AbstractUser):
-    # O campo 'username' é fornecido por AbstractUser, não precisa ser redeclarado aqui.
     email = models.EmailField(unique=True)
     first_name = models.CharField(max_length=100, blank=True)
     last_name = models.CharField(max_length=100, blank=True)
-    birth_date = models.DateField(null=False, blank=False)
+    birth_date = models.DateField(null=False, blank=False, default = datetime.date.today())
     phone_number = models.CharField(max_length=20, null=True, blank=True)
     image = models.ImageField(upload_to="users_profile_pics/", blank=True, null=True)
 
