71f914c32bd1607db64389b56f2066654e8db3cf
davijannotti <davi29012004@gmail.com>
2025-08-05 10:54:35 -0300

    Add admin configurations for auctions and users models

--- auctions/admin.py
+++ auctions/admin.py
@@ -1,3 +1,37 @@
 from django.contrib import admin
+from .models import Auction, Bid, Category, Item
 
-# Register your models here.
+
+@admin.register(Auction)
+class AuctionAdmin(admin.ModelAdmin):
+    list_display = (
+        "name",
+        "item",
+        "owner",
+        "start_time",
+        "end_time",
+        "current_price",
+        "status",
+    )
+    list_filter = ("status", "owner")
+    search_fields = ("name", "item__name")
+
+
+@admin.register(Bid)
+class BidAdmin(admin.ModelAdmin):
+    list_display = ("user", "auction", "amount", "created_at")
+    list_filter = ("auction",)
+    search_fields = ("user__username", "auction__name")
+
+
+@admin.register(Category)
+class CategoryAdmin(admin.ModelAdmin):
+    list_display = ("name",)
+    search_fields = ("name",)
+
+
+@admin.register(Item)
+class ItemAdmin(admin.ModelAdmin):
+    list_display = ("name", "category", "starting_bid", "max_bid")
+    list_filter = ("category",)
+    search_fields = ("name",)
--- users/models.py
+++ users/models.py
@@ -7,7 +7,9 @@ class User(AbstractUser):
     email = models.EmailField(unique=True)
     first_name = models.CharField(max_length=100, blank=True)
     last_name = models.CharField(max_length=100, blank=True)
-    birth_date = models.DateField(null=False, blank=False, default = datetime.date.today())
+    birth_date = models.DateField(
+        null=False, blank=False, default=datetime.date.today()
+    )
     phone_number = models.CharField(max_length=20, null=True, blank=True)
     image = models.ImageField(upload_to="users_profile_pics/", blank=True, null=True)
 
