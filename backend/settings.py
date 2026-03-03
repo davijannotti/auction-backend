a708179543bad7b8d9bcd5f9e48f5bda9f1b59f2
Vinícius de Souza Oliveira <viniso1903@gmail.com>
2026-03-03 17:43:29 -0300

    settings.py updated to allow react url

--- backend/settings.py
+++ backend/settings.py
@@ -150,6 +150,8 @@ REST_FRAMEWORK = {
 CORS_ALLOWED_ORIGINS = [
     "http://localhost:3000",
     "http://127.0.0.1:3000",
+    "http://localhost:5173",
+    "http://127.0.0.1:5173",
 ]
 
 MEDIA_URL = '/media/'
